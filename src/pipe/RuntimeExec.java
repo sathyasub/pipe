2632afa6dcf456c0f45d4ea4ebb1a7ab4f0fa4e6
Sathya Subramani”git config --global user.email sathya.subramani@thoughtworks.com” <sathya.subramani@thoughtworks.com>
2019-03-08 22:53:16 +0530

    First commit

---
+++ src/pipe/RuntimeExec.java
@@ -0,0 +1,27 @@
+package pipe;
+import java.io.*;
+public class RuntimeExec {
+
+// this is where the action is
+public static void main(String[] args) throws IOException {
+//	 ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", "eqwdkjbwekjfbkjbd");
+//     builder.redirectErrorStream(true);
+//     Process p = builder.start();
+//     BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+//     String line;
+//     line = r.readLine();
+//     System.out.println(line);
+	String event="Hire";
+     Console console = System.console();
+     console.printf(event, "dsfd");
+//	String line="wefwqef";
+//	ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", "gradle -Dtest.single=testng_AUS execute tests");
+//	builder.redirectErrorStream(true);
+//	Process p = builder.start();
+//	BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+//	line = r.readLine();
+//	System.out.println(line);
+	
+	
+            }
+}
